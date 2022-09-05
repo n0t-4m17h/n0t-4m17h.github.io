30718d916cc627e8909dd64653b84684808eb83b
n0t-4m17h <kovoor71@gmail.com>
2022-09-05 13:49:30 +1000

    MUI study: transitions, styled...

--- src/components/RandomStuff.js
+++ src/components/RandomStuff.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react'
 import Button from '@mui/material/Button';
+import { AddCircleOutline} from '@mui/icons-material';
+import RemoveCircleOutline from '@mui/icons-material/RemoveCircleOutline';
 
 import { ThemeProvider } from '@mui/material'; // read -> https://mui.com/material-ui/customization/theming/
 import { createTheme } from '@mui/material';
 import {Typography} from '@mui/material';
 import { green } from '@mui/material/colors';
+import { styled } from '@mui/material';
 
 
-// TYPOGRAPHY related
+// TYPOGRAPHY RELATED
 const { palette } = createTheme();
 const { augmentColor } = palette;
 const createColor = (mainColor) => augmentColor({color: {main: mainColor} });
@@ -45,19 +48,29 @@ const sampleTheme = createTheme({
         ceruleanBlue: createColor('#98B4D4'),
         greens: {
             main: green['A700'],
-            contrastText: '#FFF' 
+            contrastText: '#FFF',
         },
         primary: {
             main: green[500],
         },
     },
-    transitions: {
-
-    }
 });
 
 
-
+// the applied MUI Button transforms (see how below) upon mouseEnter and reverts upon mouseLeave
+const HoverButton = styled(Button)(({ theme }) => ({
+    cursor: 'pointer',
+    backgroundColor: theme.palette.ceruleanBlue,
+    // This 'transition' section allows for smooth movement
+    transition: theme.transitions.create(['backgroundColor', 'transform'], {
+        duration: theme.transitions.duration.complex,
+    }),    
+    '&:hover': {
+        backgroundColor: theme.palette.steelBlue.main,
+        transform: "scale(1.1)"
+        // transform: "scale(1.2, 1.1) translate(25%, -25%) rotateX(3.14rad) rotateY(3.14rad)"
+    }
+}));
 
 
 function CounterPlusMinus() {
@@ -66,7 +79,7 @@ function CounterPlusMinus() {
     return (
         // instead of a "<div> </div>", we can use a fragment ("<>...</>") here, since a div would just add to DOM's load. 
         <ThemeProvider theme={sampleTheme}>
-            <Button size="small" color="greens" variant="contained"
+            <Button size="small" color="greens" variant="contained" startIcon={<AddCircleOutline />}
                 onClick={
                     () => {
                         setCounter((prevCount) => prevCount + 1);
@@ -79,27 +92,15 @@ function CounterPlusMinus() {
             {/* <big> {counter} </big> */}
             <Typography variant="caption"> {counter} </Typography>
 
-            <Button size="small" color="ceruleanBlue" variant="contained" 
+            <HoverButton size="small" color="ceruleanBlue" variant="contained" endIcon={<RemoveCircleOutline />}
                 onClick={
                     () => {
                         setCounter((prevCount) => prevCount - 1);
                         console.log("decrementing ctr");
                     }
-                }
-                onMouseEnter={
-                    () => {
-                        // setCounter((prevCount) => prevCount = initCtr);
-                        // textTransform.apply(Scale(1))
-                        console.log("expanding up");
-                    }
-                } 
-                onMouseLeave={
-                    () => {
-                        console.log("concaving back down");
-                    }
                 }>
                 MINUS
-            </Button>
+            </HoverButton>
         </ThemeProvider>
     );
 }
@@ -125,7 +126,7 @@ function ChangeColor() {
 function SearchBar() {
     return (
         <div className="search">
-            {/* You need a js/css file that holds Search's style, refer to https://www.youtube.com/watch?v=b9eMGE7QtTk&list=LL&index=15&t=3579s&ab_channel=JavaScriptMastery*/}
+            {/* You need a js file that holds Search's style, refer to https://www.youtube.com/watch?v=b9eMGE7QtTk&list=LL&index=15&t=3579s&ab_channel=JavaScriptMastery*/}
             <input placeholder='Name/Ticker symbol'
                 onChange={() => {}} />
             <img src={null} alt='Search'
